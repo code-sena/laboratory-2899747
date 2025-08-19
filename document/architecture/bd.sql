d189eb3bfeda9bea99a8c26dc61eab7ffde4817d
samuel <samupalo3@gmail.com>
2025-08-19 09:39:14 -0500

    feat HU-4

--- document/architecture/bd.sql
+++ document/architecture/bd.sql
@@ -2,3 +2,25 @@
 DROP DATABASE IF EXISTS shopping_cart;
 CREATE DATABASE shopping_cart;
 USE shopping_cart;
+
+-- Tables
+CREATE TABLE payment (
+  id INT NOT NULL AUTO_INCREMENT,
+  order_id INT NOT NULL,
+  paymment_method_id INT NOT NULL,
+  amount DECIMAL(10, 2) NOT NULL,
+  payment_date DATETIME NOT NULL,
+  status VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (order_id) REFERENCES order(id),
+  FOREIGN KEY (payment_method_id) REFERENCES payment_method(id)
+);
+
+
+create table city (
+    id int primary key auto_increment,
+    name varchar(100) not null unique,
+    active boolean default true,
+    department_id int not null,
+    foreign key (department_id) references department(id)
+);
