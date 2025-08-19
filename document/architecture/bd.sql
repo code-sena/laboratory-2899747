7ce69e2ddac5a0534dd581c6542702bd018187c3
mafelosada <mafelosi2202@gmail.com>
2025-08-19 10:54:24 -0500

    feat(inventory): add database schema, class diagram and use case diagram

--- document/architecture/bd.sql
+++ document/architecture/bd.sql
@@ -2,3 +2,11 @@
 DROP DATABASE IF EXISTS shopping_cart;
 CREATE DATABASE shopping_cart;
 USE shopping_cart;
+
+CREATE TABLE Inventory (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    last_update DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (product_id) REFERENCES Product(id)
+);
