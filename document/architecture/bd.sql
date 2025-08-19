2351b6fdd4f71794c9a4653d28f144fa919b6b3d
Jhampier <ortizjhampier@gmail.com>
2025-08-19 09:42:09 -0500

    doc-add-producttracking-use-case-diagram

--- document/architecture/bd.sql
+++ document/architecture/bd.sql
@@ -2,3 +2,14 @@
 DROP DATABASE IF EXISTS shopping_cart;
 CREATE DATABASE shopping_cart;
 USE shopping_cart;
+
+-- Tables
+CREATE TABLE ProductTracking (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  shipment_id INT NOT NULL,
+  shipment_status_id INT NOT NULL,
+  location VARCHAR(255),
+  update_time DATETIME,
+  FOREIGN KEY (shipment_id) REFERENCES Shipment(id),
+  FOREIGN KEY (shipment_status_id) REFERENCES ShipmentStatus(id)
+);
