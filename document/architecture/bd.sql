e10a92f0bbe294dc0ab1e81746ecb9b8f4e6ae23
Racinger Prada <pradaracinger@gmail.com>
2025-08-19 09:50:40 -0500

    feat(docs): add User use case and table structur for HU-21

--- document/architecture/bd.sql
+++ document/architecture/bd.sql
@@ -2,3 +2,12 @@
 DROP DATABASE IF EXISTS shopping_cart;
 CREATE DATABASE shopping_cart;
 USE shopping_cart;
+
+CREATE TABLE Users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    role VARCHAR(50) NOT NULL,
+    active
+);
