3ccfc6b9fdc604b2cbe1c86e24b0ff5acbf9ef33
mafelosada <mafelosi2202@gmail.com>
2025-08-19 11:00:28 -0500

    feat(inventory): add database schema, class diagram and use case diagram

--- document/architecture/bd.sql
+++ document/architecture/bd.sql
@@ -97,3 +97,11 @@ CREATE TABLE customer (
     active BOOLEAN DEFAULT TRUE
 );
 
+CREATE TABLE Inventory (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    last_update DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (product_id) REFERENCES Product(id)
+);
+
