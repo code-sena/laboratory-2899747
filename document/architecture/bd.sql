6eaa2e55bd0ef10191710d0cd26e66de597b7ad2
Racinger Prada <pradaracinger@gmail.com>
2025-08-19 11:12:21 -0500

    Merge remote-tracking branch 'origin/develop' into feat/H21-User

--- document/architecture/bd.sql
+++ document/architecture/bd.sql
@@ -3,21 +3,3 @@ DROP DATABASE IF EXISTS shopping_cart;
 CREATE DATABASE shopping_cart;
 USE shopping_cart;
 
-CREATE TABLE Users (
-    Id INT AUTO_INCREMENT PRIMARY KEY,
-    UserName VARCHAR(100) NOT NULL UNIQUE,
-    Password VARCHAR(255) NOT NULL,
-    Email VARCHAR(100) NOT NULL UNIQUE,
-    Role VARCHAR(50) NOT NULL,
-    Active BOOLEAN NOT NULL DEFAULT TRUE
-);
-
-CREATE TABLE ProductTracking (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  shipment_id INT NOT NULL,
-  shipment_status_id INT NOT NULL,
-  location VARCHAR(255),
-  update_time DATETIME,
-  FOREIGN KEY (shipment_id) REFERENCES Shipment(id),
-  FOREIGN KEY (shipment_status_id) REFERENCES ShipmentStatus(id)
-);
